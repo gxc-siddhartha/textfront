deb6f187d8a6de7d139ed4bb2471f4d96d2fd746
Siddhartha Srivastava <siddharthasrivastava@Siddharthas-MacBook-Pro.local>
2024-12-27 11:47:07 +0530

    Desktop Build -1

--- src/pages/primary.js
+++ src/pages/primary.js
@@ -1,22 +1,22 @@
 import React, { createContext, useRef, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-
+import './key.css';
 import TArea from '../components/textarea';
 import Title from '../components/title';
 import { gsap } from "gsap";
-
+import { useNavigate } from "react-router-dom";
 export const ParentDivContext = createContext();
 
 export default function PrimaryPage() {
+    const navigate = useNavigate();
     const { key: api_key } = useParams();
-
-
     const masterContainerRef = useRef(null); // Create a ref for the container
-
     useEffect(() => {
         // Initial animation (page fade-in)
         gsap.fromTo(masterContainerRef.current, { opacity: 0, y: 0, filter: "blur(20px)" }, { opacity: 1, y: 0, duration: 0.5, ease: "power2.out", filter: "blur(0px)" });
     }, []);
+
+
 
     return (
         <ParentDivContext.Provider value={masterContainerRef}>
@@ -30,6 +30,18 @@ export default function PrimaryPage() {
                 <Title />
                 <TArea apiKey={api_key} />
             </div>
+
+            <div id="ts-container" className='flex flex-col h-screen w-screen justify-center items-center'>
+                <div className="flex opacity-75">
+                    <h1 class="text-4xl font-extrabold tracking-tight">Text</h1>
+                    <h1 class="text-4xl font-medium tracking-tight">Front</h1>
+                </div>
+                <p className='w-2/5 text-center mt-2 opacity-60'>Sorry! we currently only support desktops and laptops. 😭</p>
+            </div>
         </ParentDivContext.Provider>
     )
 }
+
+
+
+
